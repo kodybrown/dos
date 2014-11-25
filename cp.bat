a364e14fa51f5e8e68d7a21b526bb3a676011990
Kody Brown <thewizard@wasatchwizard.com>
2014-11-25 12:48:07 -0700

    Added recently improved parsing code.

--- cp.bat
+++ cp.bat
@@ -3,9 +3,12 @@
 
 :init
     set cmds=
+    set showname=
+
     if not "%~1"=="" goto :parse
+
     copy /?
-    @endlocal && @exit /B 0
+    endlocal && exit /B 0
 
 :parse
     if "%~1"=="" goto :main
@@ -13,11 +16,43 @@
     set arg=%~1
     rem echo !arg:~0,1!
 
+    rem If the argument contains '~/' then replace it with %UserProfile%
+    if "!arg:~0,2!"=="~\" (
+        set arg=!arg:~/=%UserProfile%!
+        echo arg=!arg!
+    )
+
+    :: My common shorcut/replacements..
+    if "!arg:~0,2!"=="//" (
+        set arg=%UserProfile%\!arg:~2!
+        set showname=true
+    )
+    if "!arg:~0,3!"=="/b/" (
+        set arg=%BIN%\!arg:~3!
+        set showname=true
+    )
+    if "!arg:~0,3!"=="/w/" (
+        set arg=%UserProfile%\Desktop\!arg:~3!
+        set showname=true
+    )
+    if "!arg:~0,3!"=="/e/" (
+        set arg=%UserProfile%\Desktop\!arg:~3!
+        set showname=true
+    )
+    if "!arg:~0,3!"=="/d/" (
+        set arg=%UserProfile%\Documents\!arg:~3!
+        set showname=true
+    )
+
     :: If the argument doesn't start with a '/' or '-' then
     :: replace all '/' characters with '\'..
     if not "!arg:~0,1!"=="/" (
         if not "!arg:~0,1!"=="-" (
+            :: Replace dlb-backslashes with a single back-slash
+            set arg=!arg:\\=\!
+            :: Replace forward-slashes with back-slashes
             set arg=!arg:/=\!
+	        set showname=true
         )
     )
 
@@ -52,12 +87,16 @@
     )
 
     set cmds=%cmds% %arg%
-    shift
 
+    shift
     goto :parse
 
 :main
-    rem echo %cmds%
-    copy %cmds%
-    @endlocal && @exit /B %errorlevel%
+    rem left-trim the input
+    for /f "tokens=* delims= " %%a in ("!cmds!") do set cmds=%%a
 
+	if defined showname echo copy !cmds! && set "showname="
+
+    copy %cmds%
+
+    @endlocal && @exit /B %errorlevel%
