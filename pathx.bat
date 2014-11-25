bf66df5dbf6b17fb64d94b84f3b48e47526c0a2f
Kody Brown <thewizard@wasatchwizard.com>
2014-11-25 12:58:59 -0700

    Added support for grep'ing the path values when an argument is specified.

--- pathx.bat
+++ pathx.bat
@@ -1,1 +1,2 @@
-@for %%G in ("%path:;=" "%") do @if not "%%~G"=="" echo %%~G
+@if "%~1"=="" for %%G in ("%path:;=" "%") do @if not "%%~G"=="" echo %%~G
+@if not "%~1"=="" ( for %%G in ("%path:;=" "%") do @if not "%%~G"=="" echo %%~G ) | findstr /I %~1
