f681f1959a6bc4dff26dfc65d07d68841c713cf3
Kody Brown <thewizard@wasatchwizard.com>
2014-11-25 12:57:53 -0700

    Added recently improved parsing code.

--- history.bat
+++ history.bat
@@ -1,15 +1,19 @@
 @echo off
 
-if not defined __DOSKEY_HISTORYFILE set __DOSKEY_HISTORYFILE=%Profile%\.doskey_history
-set __DOSKEY_HISTORYFILE_BACKUP=%Profile%\Settings\AutoBackup\doskey_history
-
-set doskey=C:\Windows\System32\doskey.exe
-set sort=%tools%\mingw\msys\1.0\bin\sort.exe
-set __verbose=
-set __backuponsave=true
-set __sessiononly=
+if not defined __HISTORY_FILE set __HISTORY_FILE=%Profile%\.dos_history
+set __HISTORY_FILE_BACKUP=%Profile%\Settings\AutoBackup\dos_history
 
 :init
+    set __doskey=C:\Windows\System32\doskey.exe
+    rem set __sort=%tools%\mingw\msys\1.0\bin\sort.exe
+    set __sort=%msys%\sort.exe
+    set __cat=%bin%\cat.exe
+    set __grep=%bin%\grep.exe
+
+    set "__verbose="
+    set "__backuponsave=true"
+    set "__sessiononly="
+
     call :setdatevars
     call :settimevars
 
@@ -21,19 +25,19 @@ set __sessiononly=
     if "%~1"=="-verbose" set __verbose=true && shift && goto :main
     if "%~1"=="--verbose" set __verbose=true && shift && goto :main
 
-    if "%~1"=="-edit" call :edithistory && exit /B 0
-    if "%~1"=="--edit" call :edithistory && exit /B 0
+    if "%~1"=="-edit" call :edithistory && goto :end
+    if "%~1"=="--edit" call :edithistory && goto :end
 
-    if "%~1"=="-save" call :savehistory "%~2" && exit /B 0
-    if "%~1"=="--save" call :savehistory "%~2" && exit /B 0
+    if "%~1"=="-save" call :savehistory "%~2" && goto :end
+    if "%~1"=="--save" call :savehistory "%~2" && goto :end
 
-    if "%~1"=="-backup" call :backuphistory "%~2" && exit /B 0
-    if "%~1"=="--backup" call :backuphistory "%~2" && exit /B 0
+    if "%~1"=="-backup" call :backuphistory "%~2" && goto :end
+    if "%~1"=="--backup" call :backuphistory "%~2" && goto :end
 
-    rem if "%~1"=="-del" call :deletehistory "%~2" && exit /B 0
-    rem if "%~1"=="--del" call :deletehistory "%~2" && exit /B 0
-    rem if "%~1"=="-delete" call :deletehistory "%~2" && exit /B 0
-    rem if "%~1"=="--delete" call :deletehistory "%~2" && exit /B 0
+    rem if "%~1"=="-del" call :deletehistory "%~2" && goto :end
+    rem if "%~1"=="--del" call :deletehistory "%~2" && goto :end
+    rem if "%~1"=="-delete" call :deletehistory "%~2" && goto :end
+    rem if "%~1"=="--delete" call :deletehistory "%~2" && goto :end
 
     if "%~1"=="-ss" set __sessiononly=true && shift && goto :main
     if "%~1"=="-sess" set __sessiononly=true && shift && goto :main
@@ -42,10 +46,20 @@ set __sessiononly=
     if "%~1"=="--sess" set __sessiononly=true && shift && goto :main
     if "%~1"=="--session" set __sessiononly=true && shift && goto :main
 
-    if "%~1"=="-load" call :loadhistory && exit /B 0
-    if "%~1"=="--load" call :loadhistory && exit /B 0
+    if "%~1"=="-load" call :loadhistory && goto :end
+    if "%~1"=="--load" call :loadhistory && goto :end
 
     call :listhistory "%~1"
+
+:end
+    call :deldatevars
+    call :deltimevars
+
+    set "__doskey="
+    set "__sort="
+    set "__cat="
+    set "__grep="
+
     exit /B 0
 
 :usage
@@ -59,49 +73,49 @@ set __sessiononly=
     echo   --edit        Opens the `%%__DOSKEY_MACROFILE%%` in Notepad2.
     echo                 Be sure to `alias --load` changes when finished.
     echo.
-    echo   --save        Saves the current history to `%%__DOSKEY_HISTORYFILE%%`,
-    echo                 only if the `doskey_history_loaded` envar is present.
-    if defined doskey_history_loaded echo                 Currently the `doskey_history_loaded` envar is present.
-    if not defined doskey_history_loaded echo                 Currently the `doskey_history_loaded` envar is not present.
+    echo   --save        Saves the current history to `%%__HISTORY_FILE%%`,
+    echo                 only if the `dos_history_loaded` envar is present.
+    if defined dos_history_loaded echo                 Currently the `dos_history_loaded` envar is present.
+    if not defined dos_history_loaded echo                 Currently the `dos_history_loaded` envar is not present.
     echo.
     echo   --backup      Saves current macros, then creates a backup copy of it, using
     echo                 the current date/time stamp in the file name.
     echo.
     echo   --load        Updates the current set of commands in the current processes
-    echo                 history, with those from the `%%__DOSKEY_HISTORYFILE%%` file.
+    echo                 history, with those from the `%%__HISTORY_FILE%%` file.
     echo.
     echo USAGE: history.bat query
     echo.
     echo                 Lists all commands executed, that contain `query` in it.
     echo.
     echo The saved history file is located at:
-    echo   `%__DOSKEY_HISTORYFILE%`
+    echo   `%__HISTORY_FILE%`
     goto :eof
 
 :listhistory
     if defined __sessiononly call :listsessionhistory "%~1" && goto :eof
 
-    if "%~1"=="" call cat "%__DOSKEY_HISTORYFILE%"
-    if not "%~1"=="" call cat "%__DOSKEY_HISTORYFILE%" | grep -i "%~1"
+    if "%~1"=="" call "%__cat%" "%__HISTORY_FILE%"
+    if not "%~1"=="" call "%__cat%" "%__HISTORY_FILE%" | "%__grep%" -i "%~1"
     goto :eof
 
     :listsessionhistory
-        if "%~1"=="" echo 1 && doskey /history
-        if not "%~1"=="" doskey /history |grep -i "%~1"
+        if "%~1"=="" echo 1 && "%__doskey%" /history
+        if not "%~1"=="" "%__doskey%" /history | "%__grep%" -i "%~1"
         goto :eof
 
 :edithistory
     where "Notepad2" >NUL
-    if %errorlevel% EQU 0 start "notepad2" /WAIT "notepad2.exe" "%__DOSKEY_HISTORYFILE%"
-    if %errorlevel% NEQ 0 start "notepad" /WAIT "notepad.exe" "%__DOSKEY_HISTORYFILE%"
+    if %errorlevel% EQU 0 start "notepad2" /WAIT "notepad2.exe" "%__HISTORY_FILE%"
+    if %errorlevel% NEQ 0 start "notepad" /WAIT "notepad.exe" "%__HISTORY_FILE%"
     goto :eof
 
 :loadhistory
-    set doskey_history_loaded=true
+    set dos_history_loaded=true
     goto :eof
 
 :backuphistory
-    if not defined doskey_history_loaded (
+    if not defined dos_history_loaded (
         echo HISTORY has not been loaded.
         echo I cannot backup.
         goto :eof
@@ -111,8 +125,8 @@ set __sessiononly=
     rem     call :savehistory no-backup
     rem )
 
-    set BackupFile=%__DOSKEY_HISTORYFILE_BACKUP%.%yy%%mm%%dd%-%hh%%nn%%ss%.txt
-    copy /Y "%__DOSKEY_HISTORYFILE%" "%BackupFile%" >NUL 2>&1
+    set BackupFile=%__HISTORY_FILE_BACKUP%.%yy%%mm%%dd%-%hh%%nn%%ss%.txt
+    copy /Y "%__HISTORY_FILE%" "%BackupFile%" >NUL 2>&1
 
     if not "%~1"=="no-save" (
         if defined __verbose echo history file was BACKED-UP.
@@ -121,7 +135,7 @@ set __sessiononly=
     goto :eof
 
 :savehistory
-    if not defined doskey_history_loaded (
+    if not defined dos_history_loaded (
         echo HISTORY has not been loaded.
         echo I cannot save.
         goto :eof
@@ -131,10 +145,10 @@ set __sessiononly=
     set tmp=%TEMP%\history.%yy%-%mm%-%dd%-%hh%-%nn%-%ss%-%ii%.tmp
 
     rem First, copy the current history file to the tmp file.
-    call copy /Y "%__DOSKEY_HISTORYFILE%" "%tmp%" >NUL
+    call copy /Y "%__HISTORY_FILE%" "%tmp%" >NUL
 
     rem Append, the current session's history to the tmp file.
-    call "%doskey%" /history >>"%tmp%"
+    call "%__doskey%" /history >>"%tmp%"
     if %errorlevel% NEQ 0 (
         echo HISTORY failed to export.
         echo history file was left UNCHANGED.
@@ -142,9 +156,10 @@ set __sessiononly=
         goto :eof
     )
 
-    rem Sort and remove the duplicate lines.
-    rem Outputting the results to the history file.
-    call "%sort%" --ignore-case --unique --output="%__DOSKEY_HISTORYFILE%" "%tmp%"
+    rem rem Sort and remove the duplicate lines.
+    rem rem Outputting the results to the history file.
+    rem call "%__sort%" --ignore-case --unique --output="%__HISTORY_FILE%" "%tmp%"
+    call move "%tmp%" "%__HISTORY_FILE%" >NUL 2>&1
 
     if exist "%tmp%" del /Q /F "%tmp%" >NUL 2>&1
 
@@ -160,7 +175,7 @@ set __sessiononly=
     goto :eof
 
 :deletehistory
-    call "%doskey%" %~1=
+    call "%__doskey%" %~1=
     if %errorlevel% EQU 0 (
         if defined verbose echo DELETED command from history.
     ) else (
@@ -178,6 +193,13 @@ set __sessiononly=
     )
     goto :eof
 
+:deldatevars
+    rem removes global variables
+    set "mm="
+    set "dd="
+    set "yy="
+    goto :eof
+
 :settimevars
     :: sets global variables to the current time (hh, nn, ss, ii)
     for /f "tokens=1-4 delims=:. " %%A in ("%time: =0%") do (
@@ -186,6 +208,14 @@ set __sessiononly=
         set ss=%%C
         set ii=%%D
     )
+    goto :eof
+
+:deltimevars
+    rem removes global variables
+    set "hh="
+    set "nn="
+    set "ss="
+    set "ii="
     goto :eof
 
 :incrementcount
